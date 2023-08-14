42c920cc1c965fb61ea5800b693396ddf1d4e772
Ajay Sathish Preetha <95515701+Aj-Cdr@users.noreply.github.com>
2023-08-14 14:04:09 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+
 import Main from "./screens/Main";
+import HomeScreen from './screens/Home'
+
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native'
+
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <Main />
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name = "Home" component = {HomeScreen} />
+        <Stack.Screen name = "Main" component = {Main} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
 }
